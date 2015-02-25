d23d3a89b353a5f1da7f04dd4159e56110cdb6a6
Theo Schlossnagle <jesus@omniti.com>
2015-02-25 12:54:26 -0500

    example that starts an event loop with a console

---
+++ src/examples/example1.c
@@ -0,0 +1,66 @@
+#include <mtev_defines.h>
+#include <mtev_conf.h>
+#include <mtev_console.h>
+#include <mtev_listener.h>
+#include <mtev_main.h>
+#include <mtev_memory.h>
+#include <mtev_rest.h>
+#include <eventer/eventer.h>
+
+#include <stdio.h>
+#include <getopt.h>
+
+#define APPNAME "example1"
+static char *config_file = NULL;
+static int debug = 0;
+static int foreground = 0;
+static char *droptouser = NULL, *droptogroup = NULL;
+
+static int
+usage(const char *prog) {
+	fprintf(stderr, "%s <-c conffile> [-D] [-d]\n\n", prog);
+  fprintf(stderr, "\t-c conffile\tthe configuration file to load\n");
+  fprintf(stderr, "\t-D\t\trun in the foreground (don't daemonize)\n");
+  fprintf(stderr, "\t-d\t\tturn on debugging\n");
+  return 2;
+}
+static void
+parse_cli_args(int argc, char * const *argv) {
+  int c;
+  while((c = getopt(argc, argv, "c:Dd")) != EOF) {
+    switch(c) {
+      case 'c':
+        config_file = optarg;
+        break;
+      case 'd': debug = 1; break;
+      case 'D': foreground = 1; break;
+    }
+  }
+}
+static int
+child_main() {
+  /* reload out config, to make sure we have the most current */
+
+  if(mtev_conf_load(config_file) == -1) {
+    mtevL(mtev_error, "Cannot load config: '%s'\n", config_file);
+    exit(2);
+  }
+  eventer_init();
+  mtev_console_init(APPNAME);
+  mtev_http_rest_init();
+  mtev_listener_init(APPNAME);
+
+  /* Lastly, spin up the event loop */
+  eventer_loop();
+  return 0;
+}
+int main(int argc, char **argv) {
+  parse_cli_args(argc, argv);
+  if(!config_file) exit(usage(argv[0]));
+  
+  mtev_memory_init();
+  mtev_main(APPNAME, config_file, debug, foreground,
+            MTEV_LOCK_OP_LOCK, NULL, droptouser, droptogroup,
+            child_main);
+  return 0;
+}
