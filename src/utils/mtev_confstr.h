50839aaa4d301ef60c21f6cb84b2ae2d99a42293
Aidan Cully <aidan.cully@circonus.com>
2016-04-18 13:51:11 -0400

    added mtev_confstr_parse_time_gm routine.
    parses rfc3339 datespecs.
    
    also, for consistency, changed existing mtev_confstr to return
    MTEV_CONFSTR_PARSE result codes.

--- src/utils/mtev_confstr.h
+++ src/utils/mtev_confstr.h
@@ -9,6 +9,11 @@
 
 typedef struct _mtev_duration_definition_t mtev_duration_definition_t;
 
+/* return codes from mtev_confstr_parse routines */
+#define MTEV_CONFSTR_PARSE_SUCCESS (0)
+#define MTEV_CONFSTR_PARSE_ERR_UNREPRESENTABLE (-1)
+#define MTEV_CONFSTR_PARSE_ERR_FORMAT (-2)
+
 API_EXPORT(const mtev_duration_definition_t *) mtev_get_durations_ns(void);
 API_EXPORT(const mtev_duration_definition_t *) mtev_get_durations_us(void);
 API_EXPORT(const mtev_duration_definition_t *) mtev_get_durations_ms(void);
@@ -19,6 +24,8 @@ API_EXPORT(int)
 API_EXPORT(int)
   mtev_confstr_parse_duration(const char *input, u_int64_t *output,
                               const mtev_duration_definition_t *durations);
+API_EXPORT(int)
+  mtev_confstr_parse_time_gm(const char *input, u_int64_t *output);
 
 #define mtev_confstr_parse_duration_ns(input, output) \
   mtev_confstr_parse_duration(input, output, mtev_get_durations_ns())
