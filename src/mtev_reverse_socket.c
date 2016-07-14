ff0b20a6b4ade9f206a8f8343f6e354b9592ad09
Phil Maddox <philip.maddox@circonus.com>
2016-07-14 20:05:16 +0000

    Remove mtevAssert Calls When Allocating Hash Tables
    Team objected to these for now, want a more consistent solution later.

--- src/mtev_reverse_socket.c
+++ src/mtev_reverse_socket.c
@@ -1465,7 +1465,6 @@ initiate_mtev_connection(mtev_hash_table *tracking, pthread_mutex_t *tracking_lo
     mtev_hash_delete_all(ctx->sslconfig, free, free);
   else {
     ctx->sslconfig = calloc(1, sizeof(mtev_hash_table));
-    mtevAssert(ctx->sslconfig != NULL);
     mtev_hash_init(ctx->sslconfig);
   }
   mtev_hash_merge_as_dict(ctx->sslconfig, sslconfig);
@@ -1473,7 +1472,6 @@ initiate_mtev_connection(mtev_hash_table *tracking, pthread_mutex_t *tracking_lo
     mtev_hash_delete_all(ctx->config, free, free);
   else {
     ctx->config = calloc(1, sizeof(mtev_hash_table));
-    mtevAssert(ctx->config != NULL);
     mtev_hash_init(ctx->config);
   }
   mtev_hash_merge_as_dict(ctx->config, config);
