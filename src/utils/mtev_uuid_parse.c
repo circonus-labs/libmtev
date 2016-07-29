384382de03a3f60884e4ea8dc6bba088f202f061
Riley Berton <riley.berton@circonus.com>
2016-07-29 16:09:06 -0400

    implement a 2x faster uuid parse function (#118)
    * implement a 2x faster uuid parse function
    
    * base10 to base16 for multiplier values for clarity, FALLTHROUGH comments
    
    * add a few more tests for bad uuids and UPCASE

---
+++ src/utils/mtev_uuid_parse.c
@@ -0,0 +1,146 @@
+
+#include "mtev_uuid_parse.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+struct uuid {
+  uint32_t time_low;
+  uint16_t time_mid;
+  uint16_t time_hi_and_version;
+  uint16_t clock_seq;
+  uint8_t  node[6];
+};
+
+static unsigned char lut[256] = {
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // gap before first hex digit
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
+  0,1,2,3,4,5,6,7,8,9,       // 0123456789
+  0,0,0,0,0,0,0,             // :;<=>?@ (gap)
+  10,11,12,13,14,15,         // ABCDEF 
+  0,0,0,0,0,0,0,0,0,0,0,0,0, // GHIJKLMNOPQRS (gap)
+  0,0,0,0,0,0,0,0,0,0,0,0,0, // TUVWXYZ[/]^_` (gap)
+  10,11,12,13,14,15          // abcdef 
+};
+
+#define hexvalue(c) ((unsigned long)lut[(unsigned char)(c)])
+
+static inline void uuid_pack(const struct uuid *uu, uuid_t ptr)
+{
+  register uint32_t tmp;
+  register unsigned char *out = ptr;
+
+  tmp = uu->time_low;
+  out[3] = (unsigned char) tmp;
+  tmp >>= 8;
+  out[2] = (unsigned char) tmp;
+  tmp >>= 8;
+  out[1] = (unsigned char) tmp;
+  tmp >>= 8;
+  out[0] = (unsigned char) tmp;
+
+  tmp = uu->time_mid;
+  out[5] = (unsigned char) tmp;
+  tmp >>= 8;
+  out[4] = (unsigned char) tmp;
+
+  tmp = uu->time_hi_and_version;
+  out[7] = (unsigned char) tmp;
+  tmp >>= 8;
+  out[6] = (unsigned char) tmp;
+
+  tmp = uu->clock_seq;
+  out[9] = (unsigned char) tmp;
+  tmp >>= 8;
+  out[8] = (unsigned char) tmp;
+
+  memcpy(out+10, uu->node, 6);
+}
+
+/* optimized version of strtoul which ignores checking since we already did it */
+static inline unsigned long hextoul(const char *str, size_t len)
+{
+  unsigned long value = 0;
+   
+  /* we are guaranteed to not get a string longer than 8 in hex. */
+  switch(len) {
+  case 8:
+    value += (hexvalue(str[len - 8])) * 0x10000000;
+    /* FALLTHROUGH */
+  case 7:
+    value += (hexvalue(str[len - 7])) * 0x1000000;
+    /* FALLTHROUGH */
+  case 6:
+    value += (hexvalue(str[len - 6])) * 0x100000;
+    /* FALLTHROUGH */
+  case 5:
+    value += (hexvalue(str[len - 5])) * 0x10000;
+    /* FALLTHROUGH */
+  case 4:
+    value += (hexvalue(str[len - 4])) * 0x1000;
+    /* FALLTHROUGH */
+  case 3:
+    value += (hexvalue(str[len - 3])) * 0x100;
+    /* FALLTHROUGH */
+  case 2:
+    value += (hexvalue(str[len - 2])) * 0x10;
+    /* FALLTHROUGH */
+  case 1:    
+    value += (hexvalue(str[len - 1]));
+  };
+
+  return value;
+}
+
+
+int mtev_uuid_parse(const char *in, uuid_t uu)
+{
+  const char    *p;
+  int           i;
+  int           len;
+  char          buf[3] = {0};
+  struct uuid   uuid;
+  const char    *cp;
+
+
+  for (p = in, len = 0; *p != '\0'; p++, len++) {
+
+    switch (len) {
+    case 8:
+    case 13:
+    case 18:
+    case 23:
+      if (*p != '-') {
+        return -1;
+      }
+
+      break;
+    default:
+      if (isxdigit(*p) == 0) {
+        return -1;
+      }
+      break;
+    }
+  }
+
+  if (len != 36) {
+    return -1;
+  }
+
+  uuid.time_low = hextoul(in, 8);
+  uuid.time_mid = hextoul(in + 9, 4);
+  uuid.time_hi_and_version = hextoul(in+14, 4); 
+  uuid.clock_seq = hextoul(in + 19, 4); 
+  cp = in+24;
+  for (i=0; i < 6; i++) {
+    buf[0] = *cp++;
+    buf[1] = *cp++;
+    uuid.node[i] = hextoul(buf, 2); 
+  }
+
+  uuid_pack(&uuid, uu);
+  return 0;
+}
---
+++ test/uuid_test.c
@@ -0,0 +1,123 @@
+#include <mtev_uuid_parse.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define FAIL(...)                           \
+  printf("** ");                            \
+  printf( __VA_ARGS__);                     \
+  printf("\n** FAILURE\n"); \
+  exit(1);
+
+
+static mtev_hrtime_t total_time = 0;
+
+int (*uuid_parse_fn)(const char *in, uuid_t out) = NULL;
+
+
+
+int parse_both(const char *in, uuid_t out) 
+{
+  uuid_t again;
+  int x = mtev_uuid_parse(in, out);
+
+  int y = uuid_parse(in, again);
+
+  int z = memcmp(out, again, sizeof(uuid_t));
+  if (z != 0) {
+    FAIL("mtev_uuid_parse does not match uuid_parse for '%s'", in);
+    return 1;
+  }
+  return 0;
+}
+
+void parse_file()
+{
+  char uuid[UUID_STR_LEN + 1];
+  uuid_t result;
+  FILE *f = fopen("uuids.txt", "r");
+
+  char c = getc(f);
+  int i = 0;
+   
+  while ((c != EOF)) {
+    if (c == '\n') {
+
+      uuid[UUID_STR_LEN] = '\0';
+      mtev_hrtime_t now = mtev_gethrtime();
+      int x = uuid_parse_fn(uuid, result);
+      mtev_hrtime_t end = mtev_gethrtime();
+      total_time += end - now;
+      if (x != 0) {
+        FAIL("Cannot parse!");
+      }
+      i = 0;
+    } else {
+      uuid[i++] = c;
+    }
+    c = getc(f);
+  }
+  fclose(f);
+
+}
+
+int main(int argc, char **argv) 
+{
+  uuid_parse_fn = mtev_uuid_parse;
+  for (int i = 0; i < 1000000/500; i++) {
+    parse_file();
+  }
+
+  printf("* Total parse time (mtev): %llu\n", total_time);
+  uint64_t mtev_time = total_time;
+  total_time = 0;
+
+  uuid_parse_fn = uuid_parse;
+  for (int i = 0; i < 1000000/500; i++) {
+    parse_file();
+  }
+
+  printf("* Total parse time (libuuid): %llu\n", total_time);
+
+  printf("* Speedup: %3.2fx\n", (double)total_time / (double) mtev_time);
+
+  uuid_parse_fn = parse_both;
+  for (int i = 0; i < 1000000/500; i++) {
+    parse_file();
+  }
+
+  printf("* mtev_uuid_parse and uuid_parse achieve same memory\n");
+
+  /* test upcase */
+  const char *upcase = "2F711A8C-E6A1-CE2E-CC52-82AAC2906D08";
+  uuid_t uc;
+
+  if (mtev_uuid_parse(upcase, uc) != 0) {
+    FAIL("Failed to parse upcase!");
+  }
+
+  uuid_t uc2;
+  if (uuid_parse(upcase, uc2) != 0) {
+    FAIL("libuuid failed to parse upcase, wat?");
+  }
+
+  if (memcmp(uc, uc2, sizeof(uuid_t)) != 0) {
+    FAIL("libuuid and mtev parse do not match on UPCASE");
+  }
+
+  printf("* UPCASE parse succeeds\n");
+    
+  const char *broken = "2f711a8c-e6a1-ce2ecc52-82aac2906d08";
+  if (mtev_uuid_parse(broken, uc) != -1) {
+    FAIL("Expected parse failure!");
+  }
+
+  const char *broken2 = "2f711a8c-e6a1-ce2e-hc52-82aac2906d08";
+  if (mtev_uuid_parse(broken, uc) != -1) {
+    FAIL("Expected parse failure!");
+  }
+
+  printf("* Broken UUIDs expectedly fail\n");
+  
+  printf("* SUCCESS\n");
+}
