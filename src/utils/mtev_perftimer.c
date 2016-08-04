808d21299747fbe1046a9c66147af9fc9399da2e
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2016-08-04 15:39:56 -0400

    implement mtev_perftimer convenience functions

---
+++ src/utils/mtev_perftimer.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2016, Circonus, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *    * Neither the name Circonus, Inc. nor the names of its contributors
+ *      may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "mtev_perftimer.h"
+#include <ck_md.h>
+#include <time.h>
+#include <sys/time.h>
+
+/* If the compile time hasn't slected something in particular,
+ * use mtev_gethrtime as we already tried to be careful with that.
+ */
+
+typedef struct mtev_perftimer_private_t {
+  union {
+    mtev_perftimer_t opaque;
+#if defined(MTEV_PT_USE_GETHRTIME)
+    hrtime_t start;
+#elif defined(MTEV_PT_USE_CLOCK_GETTIME)
+    struct timespec start;
+#elif defined(MTEV_PT_USE_GETTIMEOFDAY)
+    struct timeval start;
+#else
+    mtev_hrtime_t start;
+#endif
+  } d;
+} mtev_perftimer_private_t;
+
+void
+mtev_perftimer_start(mtev_perftimer_t *aliased) {
+  register mtev_perftimer_private_t *impl =
+    (mtev_perftimer_private_t *)aliased;
+#if defined(MTEV_PT_USE_GETHRTIME)
+  impl->d.start = gethrtime();
+#elif defined(MTEV_PT_USE_CLOCK_GETTIME)
+  clock_gettime(CLOCK_HIRES, &impl->d.start);
+#elif defined(MTEV_PT_USE_GETTIMEOFDAY)
+  gettimeofday(&impl->d.start, NULL);
+#else
+  impl->d.start = mtev_gethrtime();
+#endif
+}
+
+int64_t
+mtev_perftimer_elapsed(mtev_perftimer_t *aliased) {
+  register int64_t rv;
+  register mtev_perftimer_private_t *impl =
+    (mtev_perftimer_private_t *)aliased;
+#if defined(MTEV_PT_USE_GETHRTIME)
+  rv = gethrtime() - impl->d.start;
+#elif defined(MTEV_PT_USE_CLOCK_GETTIME)
+  struct timespec now;
+  clock_gettime(CLOCK_HIRES, &now);
+  rv = (int64_t)now.tv_sec - (int64_t)impl->d.start.tv_sec;
+  rv *= 1000000000;
+  rv += ((int64_t)now.tv_nsec - (int64_t)impl->d.start.tv_nsec);
+#elif defined(MTEV_PT_USE_GETTIMEOFDAY)
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  rv = (int64_t)now.tv_sec - (int64_t)impl->d.start.tv_sec;
+  rv *= 1000000000;
+  rv += ((int64_t)now.tv_usec - (int64_t)impl->d.start.tv_usec) * 1000;
+#else
+  rv = mtev_gethrtime() - impl->d.start;
+#endif
+  return rv;
+}
---
+++ test/utils/perftime_spec.lua
@@ -0,0 +1,23 @@
+ffi.cdef([=[
+  typedef struct { char opaque[64]; } mtev_perftimer_t;
+  void mtev_perftimer_start(mtev_perftimer_t *);
+  int64_t mtev_perftimer_elapsed(mtev_perftimer_t *);
+  void usleep(unsigned long us);
+]=])
+
+describe("mtev_perftimer", function()
+
+  it("should load ffi", function()
+    assert.truthy(mtev ~= nil)
+  end)
+
+  it("time a 100ms sleep", function(c)
+    local perftimer = ffi.new("mtev_perftimer_t[?]", 1)
+    mtev.mtev_perftimer_start(perftimer)
+    ffi.C.usleep(100000)
+    local ns = mtev.mtev_perftimer_elapsed(perftimer)
+    assert.truthy(ns > 100000000)
+    assert.truthy(ns < 110000000)
+  end)
+
+end)
