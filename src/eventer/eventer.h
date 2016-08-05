301eb10f6d0fe3eb6421d24f3c3c7805b8184912
Philip Maddox <philip.maddox@circonus.com>
2016-08-05 17:37:29 +0000

    Update EVENTER_CHOOSE_THREAD_FOR_EVENT_FD macro
    Change EVENTER_CHOOSE_THREAD_FOR_EVENT_FD(e) from
    eventer_choose_owner(e->fd) to
    eventer_choose_owner((e)->fd+1).

--- src/eventer/eventer.h
+++ src/eventer/eventer.h
@@ -58,7 +58,7 @@
 
 #define EVENTER_DEFAULT_ASYNCH_ABORT EVENTER_EVIL_BRUTAL
 
-#define EVENTER_CHOOSE_THREAD_FOR_EVENT_FD(e) eventer_choose_owner(e->fd);
+#define EVENTER_CHOOSE_THREAD_FOR_EVENT_FD(e) eventer_choose_owner((e)->fd+1)
 
 /* All of these functions act like their POSIX couterparts with two
  * additional arguments.  The first is the mask they require to be active
