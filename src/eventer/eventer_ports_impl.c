85db7af7d7e56ea312242dcdabd9f265879e0f7c
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2016-09-02 13:39:50 +0000

    check what time it is so our comparisons are correct

--- src/eventer/eventer_ports_impl.c
+++ src/eventer/eventer_ports_impl.c
@@ -327,6 +327,7 @@ static int eventer_ports_impl_loop() {
 
     mtev_time_maintain();
 
+    gettimeofday(&__now, NULL);
     if(compare_timeval(eventer_max_sleeptime, __dyna_sleep) < 0)
       __dyna_sleep = eventer_max_sleeptime;
  
