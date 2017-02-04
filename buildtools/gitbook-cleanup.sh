e27e58efdd9d9601b48bab325060fb51b75cb0a1
Theo Schlossnagle <jesus@lethargy.org>
2017-02-03 21:29:24 -0500

    compile docs and make work on Window Linux subsystem

--- buildtools/gitbook-cleanup.sh
+++ buildtools/gitbook-cleanup.sh
@@ -6,6 +6,8 @@ if [ ! -d docs ]; then
 fi
 
 find docs -name \*.html -print -exec \
-	perl -pi -e 's/(gitbook.page.hasChanged.*)"mtime":"([^"]+)"/$1"mtime":""/g;' \
+	perl -pi.foop -e 's/(gitbook.page.hasChanged.*)"mtime":"([^"]+)"/$1"mtime":""/g;' \
 		-e 's/(gitbook.page.hasChanged.*)"time":"([^"]+)"/$1"time":""/g;' \
 	{} \;
+
+find docs -name \*.foop -exec rm {} \;
