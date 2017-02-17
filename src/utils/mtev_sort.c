1ade368f65657bcc810b4913af55c4db23f978e7
Riley Berton <riley.berton@circonus.com>
2017-02-17 00:51:52 +0000

    switch mtev_merge_sort to an iterative impl

--- src/utils/mtev_sort.c
+++ src/utils/mtev_sort.c
@@ -4,70 +4,72 @@
 #define NULL 0x0
 #endif
 
-static void mtev_sort_split(void *node, void **front, void **back, 
-                            mtev_sort_next_function next, mtev_sort_set_next_function set_next)
-{
-  void* fast;
-  void* slow;
-  if (node == NULL || next(node) == NULL) {
-    *front = node;
-    *back = NULL;
-  }
-  else {
-    slow = node;
-    fast = next(node);
- 
-    while (fast != NULL) {
-      fast = next(fast);
-      if (fast != NULL) {
-        slow = next(slow);
-        fast = next(fast);
-      }
-    }
- 
-    *front = node;
-    *back = next(slow);
-    set_next(slow, NULL);
-  }
-}
+/* 
+ * Stable, O(N log N) time complexity, no auxiliary space
+ * 
+ * see: http://www.chiark.greenend.org.uk/~sgtatham/algorithms/listsort.html 
+ */
 
-static void* mtev_sort_merge(void* a, void* b, 
-                             mtev_sort_next_function next,
-                             mtev_sort_set_next_function set_next,
-                             mtev_sort_compare_function compare)
-{
-  void* result = NULL;
- 
-  if (a == NULL) {
-    return b;
-  } else if (b == NULL) {
-    return a;
-  }
- 
-  if (compare(a, b) <= 0) {
-    result = a;
-    set_next(result, mtev_sort_merge(next(a), b, next, set_next, compare));
-  } else {
-    result = b;
-    set_next(result, mtev_sort_merge(a, next(b), next, set_next, compare));
-  }
-  return result;
-}
-
-
-void mtev_merge_sort(void **head_ptr_ptr, 
-                     mtev_sort_next_function next,
+void mtev_merge_sort(void **head_ptr_ptr,
+                     mtev_sort_next_function get_next,
                      mtev_sort_set_next_function set_next,
                      mtev_sort_compare_function compare) 
 {
-  void *head = *head_ptr_ptr; 
-  void *a;
-  void *b; 
-  if (head == NULL || next(head) == NULL) {
+  int list_size = 1, num_merges, left_size, right_size;
+  void *tail, *left, *right, *next, *head = *head_ptr_ptr; 
+  if (head == NULL || get_next(head) == NULL) {
     return;
   }
-  mtev_sort_split(head, &a, &b, next, set_next);
-  mtev_merge_sort(&a, next, set_next, compare);
-  mtev_merge_sort(&b, next, set_next, compare);
-  *head_ptr_ptr = mtev_sort_merge(a, b, next, set_next, compare);
+
+  do { // For each power of two <= list length
+    num_merges = 0;
+    left = head;
+    tail = head = NULL; 
+
+    while (left != NULL) { 
+      num_merges++;
+      right = left;
+      left_size = 0;
+      right_size = list_size;
+
+      while (right != NULL && left_size < list_size) {
+        left_size++;
+        right = get_next(right);
+      }
+
+      while (left_size > 0 || (right_size > 0 && right != NULL)) {
+        if (left_size == 0) {
+          next = right;
+          right = get_next(right);
+          right_size--;
+        }
+        else if (right_size == 0 || right == NULL) {
+          next = left;
+          left = get_next(left);
+          left_size--;
+        }
+        else if (compare(left,right) < 0) {
+          next = left;
+          left = get_next(left);
+          left_size--;
+        }
+        else {
+          next = right;
+          right = get_next(right);
+          right_size--;
+        }
+        if (tail) {
+          set_next(tail,next);  
+        }
+        else {
+          head = next;
+        }
+        tail = next;          
+      }
+      left = right;
+    }
+    set_next(tail, NULL);
+    list_size <<= 1;
+  } while (num_merges > 1); 
+  *head_ptr_ptr = head;
 }
