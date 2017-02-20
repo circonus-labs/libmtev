e91e8358bd4dae598e94b9a67d2a3eb6de025ff0
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2017-02-20 17:19:34 +0000

    move this to autoconf

--- test/mtevbusted-wrapper.sh
+++ test/mtevbusted-wrapper.sh
@@ -2,6 +2,6 @@
 
 exec ../src/luamtev \
     '-C../src/modules/mtev_lua/?.so;{package.cpath}'  \
-    '-L../src/modules/lua-support/?.lua;./lua-harness/?.lua;{package.path}' \
+    '-L../src/modules/lua-support/?.lua;./?.lua;./lua-harness/?.lua;{package.path}' \
     '-M../src/modules/' \
     ./mtevbusted-script -i lua-support/init.lua $@
