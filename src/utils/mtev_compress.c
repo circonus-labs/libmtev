dbaecbc9c3068f6d989a4a9f80776685cac2e321
Riley Berton <riley.berton@circonus.com>
2017-04-10 16:07:00 -0400

    LZ4F support on the response side (#242)
    * LZ4F support on the response side
    
    * add a curl helper you can hand to CURLOPT_WRITEFUNCTION that can decompress LZ4F responses
    * fix LZ4F handling so it actually works on the response side
    
    * add define for LZ4F_FRAMING_SIZE, fix other constants in gzip

--- src/utils/mtev_compress.c
+++ src/utils/mtev_compress.c
@@ -4,6 +4,7 @@
 #include <zlib.h>
 #include <lz4frame.h>
 
+#define LZ4F_FRAMING_SIZE 15
 #define GZIP_WINDOW_BITS 15
 #define GZIP_ENCODING 16
 #define GZIP_DEFAULT_LEVEL 3
@@ -13,6 +14,7 @@ struct mtev_stream_compress_ctx
 {
   mtev_compress_type type;
   mtev_boolean begun;
+  mtev_boolean flushed;
   LZ4F_compressionContext_t lz4_compress_ctx;
   z_stream zlib_compress_ctx;
 };
@@ -24,12 +26,19 @@ struct mtev_stream_decompress_ctx
   z_stream zlib_decompress_ctx;
 };
 
+struct mtev_decompress_curl_helper
+{
+  curl_write_callback write_function;
+  void *write_closure;
+  mtev_stream_decompress_ctx_t decompress_ctx;
+};
+
 size_t
 mtev_compress_bound(mtev_compress_type type, size_t source_len)
 {
   switch(type) {
   case MTEV_COMPRESS_LZ4F:
-    return LZ4F_compressBound(source_len, NULL);
+    return LZ4F_compressBound(source_len, NULL) + LZ4F_FRAMING_SIZE;
   case MTEV_COMPRESS_GZIP:
     return deflateBound(NULL, source_len);
   case MTEV_COMPRESS_DEFLATE:
@@ -40,19 +49,19 @@ mtev_compress_bound(mtev_compress_type type, size_t source_len)
   return 0;
 }
 
-int 
+int
 mtev_compress_gzip(const char *data, size_t len, unsigned char **compressed, size_t *compressed_len)
 {
   z_stream stream;
   size_t max_compressed_len;
   int err;
 
-  err = deflateInit2(&stream, 9, Z_DEFLATED, GZIP_WINDOW_BITS | GZIP_ENCODING, 8, Z_DEFAULT_STRATEGY);
+  err = deflateInit2(&stream, GZIP_DEFAULT_LEVEL, Z_DEFLATED, GZIP_WINDOW_BITS | GZIP_ENCODING, GZIP_DEFAULT_MEMLEVEL, Z_DEFAULT_STRATEGY);
   if (err != Z_OK) {
     mtevL(mtev_error, "mtev_http_gzip -> deflateInit2: %d\n", err);
     return err;
   }
-  
+
   stream.next_in = (Bytef *)data;
   stream.avail_in = len;
   max_compressed_len = deflateBound(&stream, len);
@@ -74,7 +83,7 @@ mtev_compress_gzip(const char *data, size_t len, unsigned char **compressed, siz
   return Z_OK;
 }
 
-int 
+int
 mtev_compress_lz4f(const char *data, size_t len, unsigned char **compressed, size_t *compressed_len)
 {
   LZ4F_compressionContext_t ctx;
@@ -86,7 +95,7 @@ mtev_compress_lz4f(const char *data, size_t len, unsigned char **compressed, siz
     return err;
   }
 
-  *compressed_len = LZ4F_compressBound(len, NULL) + 15;
+  *compressed_len = LZ4F_compressBound(len, NULL) + LZ4F_FRAMING_SIZE;
   *compressed = malloc(*compressed_len);
   if (*compressed == NULL) {
     mtevL(mtev_error, "mtev_http_lz4f: Cannot allocate compression dest\n");
@@ -122,8 +131,8 @@ mtev_compress_lz4f(const char *data, size_t len, unsigned char **compressed, siz
   return 0;
 }
 
-static int 
-_mtev_compress_deflate(const char *data, size_t len, 
+static int
+_mtev_compress_deflate(const char *data, size_t len,
                        unsigned char *compressed, size_t *compressed_len)
 {
   int err = compress2(compressed, compressed_len, (Bytef *)data, len, 9);
@@ -134,7 +143,7 @@ _mtev_compress_deflate(const char *data, size_t len,
   return 0;
 }
 
-int 
+int
 mtev_compress_deflate(const char *data, size_t len, unsigned char **compressed, size_t *compressed_len)
 {
   size_t max_compressed_len;
@@ -150,7 +159,7 @@ mtev_compress_deflate(const char *data, size_t len, unsigned char **compressed, 
 
 
 int
-mtev_compress(mtev_compress_type type, const char *data, size_t len, 
+mtev_compress(mtev_compress_type type, const char *data, size_t len,
               unsigned char **compressed, size_t *compressed_len)
 {
   switch(type) {
@@ -212,9 +221,9 @@ mtev_stream_compress_init(mtev_stream_compress_ctx_t *ctx, mtev_compress_type ty
       if (err != Z_OK) {
         mtevL(mtev_error, "mtev_stream_compress_init: Error creating gzip compression context: %d\n", err);
       }
-        
+
       return err;
-    }   
+    }
   case MTEV_COMPRESS_LZ4F:
     {
       LZ4F_errorCode_t err = LZ4F_createCompressionContext(&ctx->lz4_compress_ctx, LZ4F_VERSION);
@@ -249,7 +258,7 @@ mtev_stream_compress_lz4f(mtev_stream_compress_ctx_t *ctx, const char *source_da
     ctx->begun = mtev_true;
   }
 
-  size_t chunk = LZ4F_compressUpdate(ctx->lz4_compress_ctx, out + s, *out_len - s, 
+  size_t chunk = LZ4F_compressUpdate(ctx->lz4_compress_ctx, out + s, *out_len - s,
                                      source_data, *source_len, NULL);
   if (LZ4F_isError(chunk)) {
     mtevL(mtev_error, "mtev_stream_compress_lz4f: Error compressupdate: %s\n",
@@ -272,13 +281,13 @@ mtev_stream_compress_gzip(mtev_stream_compress_ctx_t *ctx, const char *source_da
   ctx->zlib_compress_ctx.avail_in = in_len;
   ctx->zlib_compress_ctx.next_out = out;
   ctx->zlib_compress_ctx.avail_out = *out_len;
-  
+
   size_t t = ctx->zlib_compress_ctx.total_out;
   int x = deflate(&ctx->zlib_compress_ctx, Z_NO_FLUSH);
   if (x != Z_OK) {
     mtevL(mtev_error, "mtev_stream_compress_gzip: error deflate stream: %d\n",
           x);
-    return -1;    
+    return -1;
   }
   /* order of operations matters here */
   *source_len = ctx->zlib_compress_ctx.avail_in;
@@ -286,8 +295,8 @@ mtev_stream_compress_gzip(mtev_stream_compress_ctx_t *ctx, const char *source_da
   return 0;
 }
 
-int  
-mtev_stream_compress(mtev_stream_compress_ctx_t *ctx, const char *source_data, 
+int
+mtev_stream_compress(mtev_stream_compress_ctx_t *ctx, const char *source_data,
                      size_t *len, unsigned char *out, size_t *out_len)
 {
   switch (ctx->type) {
@@ -319,25 +328,40 @@ mtev_stream_compress(mtev_stream_compress_ctx_t *ctx, const char *source_data,
 }
 
 static int
-mtev_stream_compress_flush_lz4f(mtev_stream_compress_ctx_t *ctx, 
+mtev_stream_compress_flush_lz4f(mtev_stream_compress_ctx_t *ctx,
                                 unsigned char *out, size_t *out_len)
 {
   if (ctx->begun == mtev_false) {
     return -1;
   }
-  
+
+  if (ctx->flushed == mtev_true) {
+    *out_len = 0;
+    return 0;
+  }
+
   size_t s = LZ4F_flush(ctx->lz4_compress_ctx, out, *out_len, NULL);
   if (LZ4F_isError(s)) {
     mtevL(mtev_error, "mtev_stream_compress_flush_lz4f: flush failed %s\n",
           LZ4F_getErrorName(s));
     return -1;
   }
+
+  if (s == 0) {
+    s = LZ4F_compressEnd(ctx->lz4_compress_ctx, out, *out_len, NULL);
+    if (LZ4F_isError(s)) {
+      mtevL(mtev_error, "mtev_stream_compress_flush_lz4f: flush failed %s\n",
+            LZ4F_getErrorName(s));
+      return -1;
+    }
+    ctx->flushed = mtev_true;
+  }
   *out_len = s;
   return 0;
 }
 
 static int
-mtev_stream_compress_flush_gzip(mtev_stream_compress_ctx_t *ctx, 
+mtev_stream_compress_flush_gzip(mtev_stream_compress_ctx_t *ctx,
                                 unsigned char *out, size_t *out_len)
 {
   if (ctx->begun == mtev_false) {
@@ -346,7 +370,7 @@ mtev_stream_compress_flush_gzip(mtev_stream_compress_ctx_t *ctx,
 
   size_t loop_out = 0;
   int x = 0;
-  
+
   while (x != Z_STREAM_END && *out_len - loop_out > 0) {
     size_t total_out_pre_flush = ctx->zlib_compress_ctx.total_out;
     ctx->zlib_compress_ctx.next_out = out + loop_out;
@@ -356,14 +380,14 @@ mtev_stream_compress_flush_gzip(mtev_stream_compress_ctx_t *ctx,
       mtevL(mtev_error, "mtev_stream_compress_flush_gzip: error flushing: %d\n", x);
       return x;
     }
-    loop_out += ctx->zlib_compress_ctx.total_out - total_out_pre_flush;    
+    loop_out += ctx->zlib_compress_ctx.total_out - total_out_pre_flush;
   }
   *out_len = loop_out;
   return 0;
 }
 
 int
-mtev_stream_compress_flush(mtev_stream_compress_ctx_t *ctx, 
+mtev_stream_compress_flush(mtev_stream_compress_ctx_t *ctx,
                            unsigned char *out, size_t *out_len)
 {
   switch(ctx->type) {
@@ -380,33 +404,27 @@ mtev_stream_compress_flush(mtev_stream_compress_ctx_t *ctx,
   return -1;
 }
 
-int
+static int
 mtev_stream_compress_finish_lz4f(mtev_stream_compress_ctx_t *ctx)
 {
-  size_t s = LZ4F_compressEnd(ctx->lz4_compress_ctx, NULL, 0, NULL);
-  if (LZ4F_isError(s)) {
-    mtevL(mtev_error, "mtev_stream_compress_finish_lz4f: finish failed %s\n",
-          LZ4F_getErrorName(s));
-    return -1;
-  }
   LZ4F_freeCompressionContext(ctx->lz4_compress_ctx);
   return 0;
 }
 
-int 
+static int
 mtev_stream_compress_finish_gzip(mtev_stream_compress_ctx_t *ctx)
 {
   int x;
   ctx->zlib_compress_ctx.next_out = NULL;
   ctx->zlib_compress_ctx.avail_out = 0;
   ctx->zlib_compress_ctx.next_in = NULL;
-  ctx->zlib_compress_ctx.avail_in = 0; 
+  ctx->zlib_compress_ctx.avail_in = 0;
   x = deflateEnd(&ctx->zlib_compress_ctx);
   return x;
 }
 
 int
-mtev_stream_compress_finish(mtev_stream_compress_ctx_t *ctx) 
+mtev_stream_compress_finish(mtev_stream_compress_ctx_t *ctx)
 {
   switch(ctx->type) {
   case MTEV_COMPRESS_LZ4F:
@@ -423,14 +441,14 @@ mtev_stream_compress_finish(mtev_stream_compress_ctx_t *ctx)
 }
 
 int
-mtev_stream_decompress_init(mtev_stream_decompress_ctx_t *ctx, 
+mtev_stream_decompress_init(mtev_stream_decompress_ctx_t *ctx,
                             mtev_compress_type type)
 {
   ctx->type = type;
   switch(ctx->type) {
   case MTEV_COMPRESS_LZ4F:
     {
-      LZ4F_errorCode_t err = LZ4F_createDecompressionContext(&ctx->lz4_decompress_ctx, 
+      LZ4F_errorCode_t err = LZ4F_createDecompressionContext(&ctx->lz4_decompress_ctx,
                                                              LZ4F_VERSION);
       if (LZ4F_isError(err)) {
         mtevL(mtev_error, "mtev_stream_decompress_init: error create decompression context for lz4f: %s\n", LZ4F_getErrorName(err));
@@ -440,12 +458,12 @@ mtev_stream_decompress_init(mtev_stream_decompress_ctx_t *ctx,
     }
   case MTEV_COMPRESS_GZIP:
     {
-      int err = inflateInit2(&ctx->zlib_decompress_ctx, 15 + 16);
+      int err = inflateInit2(&ctx->zlib_decompress_ctx, GZIP_WINDOW_BITS + GZIP_ENCODING);
       if (err != Z_OK) {
         mtevL(mtev_error, "mtev_stream_decompress_init: gzip error initing the zstream: %s\n",
             ctx->zlib_decompress_ctx.msg);
         return -1;
-      }      
+      }
       return 0;
     }
   default:
@@ -455,7 +473,7 @@ mtev_stream_decompress_init(mtev_stream_decompress_ctx_t *ctx,
 }
 
 static int
-mtev_stream_decompress_lz4f(mtev_stream_decompress_ctx_t *ctx, 
+mtev_stream_decompress_lz4f(mtev_stream_decompress_ctx_t *ctx,
                             const unsigned char *compressed,
                             size_t *compressed_len,
                             unsigned char *decompressed,
@@ -476,7 +494,7 @@ mtev_stream_decompress_lz4f(mtev_stream_decompress_ctx_t *ctx,
     mtevL(mtev_error, "mtev_stream_decompress_lz4f: error decompressing: %s\n",
           LZ4F_getErrorName(s));
     return -1;
-  }  
+  }
 
   *compressed_len = in_size;
   *decompressed_len = out_size;
@@ -484,7 +502,7 @@ mtev_stream_decompress_lz4f(mtev_stream_decompress_ctx_t *ctx,
 }
 
 static int
-mtev_stream_decompress_gzip(mtev_stream_decompress_ctx_t *ctx, 
+mtev_stream_decompress_gzip(mtev_stream_decompress_ctx_t *ctx,
                             const unsigned char *compressed,
                             size_t *compressed_len,
                             unsigned char *decompressed,
@@ -497,7 +515,7 @@ mtev_stream_decompress_gzip(mtev_stream_decompress_ctx_t *ctx,
   ctx->zlib_decompress_ctx.avail_in = *compressed_len;
   ctx->zlib_decompress_ctx.next_out = decompressed;
   ctx->zlib_decompress_ctx.avail_out = *decompressed_len;
-  
+
   int x = inflate(&ctx->zlib_decompress_ctx, 0);
   if (x != Z_OK && x != Z_STREAM_END) {
     mtevL(mtev_error, "mtev_stream_decompress_gzip: zlib error decompressing: %s\n",
@@ -511,7 +529,7 @@ mtev_stream_decompress_gzip(mtev_stream_decompress_ctx_t *ctx,
 
 
 int
-mtev_stream_decompress(mtev_stream_decompress_ctx_t *ctx, 
+mtev_stream_decompress(mtev_stream_decompress_ctx_t *ctx,
                          const unsigned char *compressed,
                          size_t *compressed_len,
                          unsigned char *decompressed,
@@ -544,4 +562,50 @@ mtev_stream_decompress_finish(mtev_stream_decompress_ctx_t *ctx)
     return 0;
   };
   return 0;
+}
+
+mtev_decompress_curl_helper_t *
+mtev_decompress_create_curl_helper(curl_write_callback write_function, void *closure, mtev_compress_type type)
+{
+  mtev_decompress_curl_helper_t *ch = malloc(sizeof(mtev_decompress_curl_helper_t));
+  ch->write_function = write_function;
+  ch->write_closure = closure;
+  mtev_stream_decompress_init(&ch->decompress_ctx, type);
+  return ch;
+}
+
+void
+mtev_decompress_destroy_curl_helper(mtev_decompress_curl_helper_t *ch)
+{
+  mtev_stream_decompress_finish(&ch->decompress_ctx);
+  free(ch);
+}
+
+size_t
+mtev_curl_write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+  unsigned char decompressed[8192];
+  mtev_decompress_curl_helper_t *ch = (mtev_decompress_curl_helper_t *)userdata;
+  size_t data_len = size * nmemb;
+  size_t read_compressed = 0;
+
+  while (read_compressed < data_len) {
+    size_t x = data_len - read_compressed;
+    size_t decompressed_len = sizeof(decompressed);
+
+    if (mtev_stream_decompress(&ch->decompress_ctx, (const unsigned char *)(ptr + read_compressed), &x, decompressed, &decompressed_len) == -1) {
+      mtevL(mtev_error, "Error decompressing in mtev_curl_write_callback\n");
+      return -1;
+    }
+    read_compressed += x;
+
+    /* pass along to ch->write_function */
+    size_t pos = 0;
+    while (decompressed_len > 0) {
+      size_t read = ch->write_function((char *)(decompressed + pos), 1, decompressed_len, ch->write_closure);
+      decompressed_len -= read;
+      pos += read;
+    }
+  }
+  return data_len;
 }
