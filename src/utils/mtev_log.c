e640fb980e406938f9ad744a8bd372ae923b11e4
Aidan Cully <aidan.cully@circonus.com>
2017-04-24 10:26:24 -0400

    rename mtev_log_speculate_commit to mtev_log_speculate_finish
    "commit" implies that the speculative log buffer should be displayed,
    but the implementation allows a "NULL" parameter for the output
    buffer to discard the captured data. "finish" avoids the "display"
    connotation.

--- src/utils/mtev_log.c
+++ src/utils/mtev_log.c
@@ -1848,7 +1848,7 @@ mtev_log_speculate_commit_cb(uint64_t idx, const struct timeval *tv,
 }
 
 void
-mtev_log_speculate_commit(mtev_log_stream_t ls, mtev_log_stream_t speculation)
+mtev_log_speculate_finish(mtev_log_stream_t ls, mtev_log_stream_t speculation)
 {
   if (ls) mtev_log_memory_lines(speculation, 0, mtev_log_speculate_commit_cb, ls);
   log_stream_membuf_free(speculation->op_ctx);
--- test/speculatelog_test.c
+++ test/speculatelog_test.c
@@ -7,11 +7,11 @@ int main(int argc, char *argv[])
   mtev_log_stream_t *speculation = mtev_log_speculate(100, 65536);
   for (size_t index = 0; index < 100; index++)
     mtevL(speculation, "string %s i32 %d\n", "str1", (int) index);
-  mtev_log_speculate_commit(mtev_stderr, speculation);
+  mtev_log_speculate_finish(mtev_stderr, speculation);
 
   speculation = mtev_log_speculate(100, 65536);
   for (size_t index = 0; index < 100; index++)
     mtevL(speculation, "discarded %s i32 %d\n", "str1", (int) index);
-  mtev_log_speculate_commit(NULL, speculation);
+  mtev_log_speculate_finish(NULL, speculation);
   return 0;
 }
