b6c1e140b370e6527689de14c57b419723f6ef4c
Theo Schlossnagle <jesus@lethargy.org>
2017-06-07 10:50:50 -0400

    broken mime type

--- src/mtev_rest.c
+++ src/mtev_rest.c
@@ -1027,7 +1027,7 @@ mtev_hash_store(&mime_type_defaults, strdup(ext), strlen(ext), strdup(type))
   ADD_MIME_TYPE("jpg", "image/jpg");
   ADD_MIME_TYPE("jpeg", "image/jpg");
   ADD_MIME_TYPE("svg", "image/svg+xml");
-  ADD_MIME_TYPE("json", "application/javascript");
+  ADD_MIME_TYPE("json", "application/json");
   ADD_MIME_TYPE("eot", "application/vnd.ms-fontobject");
   ADD_MIME_TYPE("ttf", "application/octet-stream");
   ADD_MIME_TYPE("woff", "application/font-woff");
