c3597d44d65738f2ab9c20662d9c942c273da7b6
Riley Berton <riley.berton@circonus.com>
2017-08-10 21:06:05 +0000

    LFU cache with O(1) operations based on bucketed lists of frequencies

---
+++ src/utils/mtev_lfu.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2017, Circonus, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name Circonus, Inc. nor the names
+ *       of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written
+ *       permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _MTEV_LFU_H
+#define _MTEV_LFU_H
+
+#include "mtev_defines.h"
+
+typedef struct mtev_lfu mtev_lfu_t;
+
+/*!
+ * \fn mtev_lfu_create(int32_t max_entries, void (*free_fn)(void *))
+ * \brief Create an LFU of max_entries size
+ *
+ * Will call free_fn when an item is evicted. if free_fn is null, call free().
+ * If max_entries == -1 then this devolves to a normal hashtable.
+ */
+API_EXPORT(mtev_lfu_t *)
+  mtev_lfu_create(int32_t max_entries, void (*free_fn)(void *));
+
+/*!
+ * \fn mtev_lfu_destroy(mtev_lfu_t *)
+ * \brief Destroy the LFU
+ */
+API_EXPORT(void)
+  mtev_lfu_destroy(mtev_lfu_t *lfu);
+
+/*!
+ * \fn mtev_lfu_invalidate(mtev_lfu_t *)
+ * \brief Remove all entries from the LFU
+ */
+API_EXPORT(void)
+  mtev_lfu_invalidate(mtev_lfu_t *lfu);
+
+/*!
+ * \fn mtev_lfu_put(mtev_lfu_t *lfu, const char *key, size_t key_len, void *value)
+ * \brief Put a new item into the LFU
+ *
+ * If some other thread has added a val at this key this will overwrite it and
+ * restart the frequency count at 1.
+ * 
+ * This will cause an eviction of the least frequently used item if the cache is full.
+ */
+API_EXPORT(mtev_boolean)
+  mtev_lfu_put(mtev_lfu_t *lfu, const char *key, size_t key_len, void *value);
+
+/*!
+ * \fn mtev_lfu_get(mtev_lfu_t *lfu, const char *key, size_t key_len)
+ * \brief Get an item from the LFU by key 
+ */
+API_EXPORT(void *)
+  mtev_lfu_get(mtev_lfu_t *lfu, const char *key, size_t key_len);
+
+/*!
+ * \fn mtev_lfu_remove(mtev_lfu_t *lfu, const char *key, size_t key_len)
+ * \brief Remove key from the LFU
+ * 
+ * This does not call the free_fn, instead it returns the value
+ */
+API_EXPORT(void *)
+  mtev_lfu_remove(mtev_lfu_t *lfu, const char *key, size_t key_len);
+
+/*!
+ * \fn mtev_lfu_size(mtev_lfu_t *lfu)
+ * \brief Return the total entry count in the LFU
+ */
+API_EXPORT(int32_t)
+  mtev_lfu_size(mtev_lfu_t *lfu);
+
+#endif
---
+++ test/lfu_test.c
@@ -0,0 +1,111 @@
+#include <mtev_lfu.h>
+#include <mtev_time.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <uuid/uuid.h>
+
+#define FAIL(...)                           \
+  printf("** ");                            \
+  printf( __VA_ARGS__);                     \
+  printf("\n** FAILURE\n"); \
+  exit(1);
+
+struct data{
+  int i;
+  char key[10];
+};
+
+void
+noop_free(void *x)
+{
+  (void)x;
+  return;
+}
+
+int main(int argc, char **argv) 
+{
+  uuid_t uuid;
+  char s[PATH_MAX];
+  srand(time(NULL));
+
+  struct data datas[10] = 
+    {
+      {0, "zero"},
+      {1, "one"},
+      {2, "two"},
+      {3, "three"},
+      {4, "four"},
+      {5, "five"},
+      {6, "six"},
+      {7, "seven"},
+      {8, "eight"},
+      {9, "nine"}
+    };
+
+  /* test max entries */
+  mtev_lfu_t *lfu = mtev_lfu_create(5, noop_free);
+  
+  for (int j = 0; j < 10; j++) {
+    mtev_lfu_put(lfu, datas[j].key, strlen(datas[j].key), &datas[j]);
+  }
+
+  if (mtev_lfu_size(lfu) != 5) {
+    FAIL("LFU size is not 5");
+  }
+
+  /* test that the last 5 entries are what remains in the LRU */
+  for (int j = 5; j < 10; j++) {
+    void *d = mtev_lfu_get(lfu, datas[j].key, strlen(datas[j].key));
+    struct data* dd = (struct data *)d;
+    if (dd->i != datas[j].i) {
+      FAIL("LFU expected %d, got %d", datas[j].i, dd->i);
+    }
+  }
+
+
+  /* now get 3 of the items to bump their frequency count up */
+  void *x = mtev_lfu_get(lfu, "five", 4);
+  void *y = mtev_lfu_get(lfu, "six", 3);
+  void *z = mtev_lfu_get(lfu, "seven", 5);
+
+  /* add zero through four items.. these will evict themselves and shouldn't touch five->nine
+   * since five->nine have higher frequency counts */
+  for (int j = 0; j < 5; j++) {
+    mtev_lfu_put(lfu, datas[j].key, strlen(datas[j].key), &datas[j]);
+  }
+
+  /* five->nine should remain */
+  void *dx = mtev_lfu_get(lfu, "five", 4);
+  void *dy = mtev_lfu_get(lfu, "six", 3);
+  void *dz = mtev_lfu_get(lfu, "seven", 5);
+  void *db = mtev_lfu_get(lfu, "nine", 4);
+
+  if (x != dx) {
+    FAIL("LFU expected five to be in cache");
+  }
+  if (y != dy) {
+    FAIL("LFU expected six to be in cache");
+  }
+  if (z != dz) {
+    FAIL("LFU expected seven to be in cache");
+  }
+  /* eight was removed by the puts on line 75 as it would have been in the lowest frequency list on the 
+   * first insert, thereafter the newly inserted item became the low freq item so it was replaced
+   *
+   * Nine should be here however.
+   */
+  if (db == NULL) {
+    FAIL("LFU expected nine to be in cache");
+  }
+
+  mtev_lfu_invalidate(lfu);
+  if (mtev_lfu_size(lfu) != 0) {
+    FAIL("LRU size is not 0");
+  }
+
+  mtev_lfu_destroy(lfu);
+  printf("SUCCESS\n");
+
+}
