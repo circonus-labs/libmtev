a047190107df22f04e0c71ef39fb4effab5a2884
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2018-06-11 13:57:32 -0400

    actually upper-case things

--- src/utils/mtev_uuid_parse.c
+++ src/utils/mtev_uuid_parse.c
@@ -25,7 +25,7 @@ static unsigned char reverse_lut[16] = {
 #define hexvalue(c) ((unsigned long)lut[(unsigned char)(c)])
 
 static unsigned char reverse_uut[16] = {
-  '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'
+  '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
 };
 
 #define HEXvalue(c) ((unsigned long)uut[(unsigned char)(c)])
