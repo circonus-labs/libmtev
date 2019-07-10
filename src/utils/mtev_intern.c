8cf6e78deee61ebe67dda57d8b9064035fa4622e
vasuraman11 <36165091+vasuraman11@users.noreply.github.com>
2019-07-10 07:29:49 -0700

    Use safe refcnt inc in mtev_intern_copy as in other places in the code (#541)

--- src/utils/mtev_intern.c
+++ src/utils/mtev_intern.c
@@ -945,8 +945,18 @@ mtev_intern_t
 mtev_intern_copy(const mtev_intern_t i) {
   if(i.opaque1 == 0) return mtev_intern_null;
   mtev_intern_internal_t *ii = (mtev_intern_internal_t *)(i.opaque1 - offsetof(mtev_intern_internal_t, v));
-  assert(ck_pr_load_32(&ii->refcnt) != 0);
-  ck_pr_inc_32(&ii->refcnt);
+  /* if we got into this function, refcnt must be >0
+     because caller has a reference to the intern,
+     except if caller misbehaves and does too many
+     releases on this intern concurrently
+     in order to assist in catching this for debug
+     we detect this misbehavior by checking for
+     concurrent releases and asserting on zero */
+  uint32_t prev;
+  while(0 != (prev = ck_pr_load_32(&ii->refcnt))) {
+    if(ck_pr_cas_32(&ii->refcnt, prev, prev+1)) break;
+  }
+  assert(prev != 0);
   return i;
 }
 
