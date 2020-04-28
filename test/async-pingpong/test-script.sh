c4b952118f1bae3acfe3470781a0b6f6af3456f7
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2020-04-28 09:54:15 -0400

    Allow for slow startup of tests.

--- test/async-pingpong/test-script.sh
+++ test/async-pingpong/test-script.sh
@@ -5,9 +5,18 @@ RV=2
 DIR=`dirname $0`
 cd $DIR
 ./test -c ./test.conf >/dev/null 2>&1 &
-sleep 0.2
-OUT=`curl -s http://127.0.0.1:8888/test`
+LOOP=5
+OUT=""
+while [ "$LOOP" -gt 0 -a "x$OUT" = "x" ]; do
+	OUT=`curl -s http://127.0.0.1:8888/test`
+        if [ -n "$OUT" ]; then
+		RV=0
+	else
+		sleep 1
+        fi
+        LOOP=$(($LOOP - 1))
+done
+
 echo "$OUT"
 kill -9 %1
-
-exit 0
+exit $RV
