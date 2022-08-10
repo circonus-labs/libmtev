f91df359e51f7b91743a90cac0e3e1d6dd4bb1b0
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2022-08-10 12:40:26 -0400

    HTTP/2 should set EVENTER_READ mask correctly.
    A misunderstanding of the nghttp2_session_send API let to relying on an
    EAGAIN-style error that is never returned from that function.  That
    function only returns success and the need to perform further reads and
    writes must be set explicitly from the session state.
    
    CIRC-8915

--- src/mtev_http2.c
+++ src/mtev_http2.c
@@ -34,6 +34,7 @@
 #include "mtev_http_private.h"
 #include "mtev_str.h"
 #include "mtev_b64.h"
+#include "mtev_stacktrace.h"
 
 #include <ctype.h>
 #include <errno.h>
@@ -1138,7 +1139,8 @@ mtev_http2_resume_all_unpaused_streams(mtev_http2_parent_session *ctx) {
       ctx->paused = H2_NORMAL;
       mtevL(h2_debug, "http2 session resuming with output data (%p -> %d)\n",
             ctx->parent, ctx->stream_id);
-      nghttp2_session_resume_data(ctx->parent->session, ctx->stream_id);
+      int rv = nghttp2_session_resume_data(ctx->parent->session, ctx->stream_id);
+      mtevL(h2_debug, "http2 session resuming completed -> %d\n", rv);
     }
   }
 }
@@ -1160,7 +1162,7 @@ mtev_http2_session_drive(eventer_t e, int origmask, void *closure,
   mask = 0;
   int rv = nghttp2_session_send(ctx->session);
   mtevL(h2_debug, "http2 drive -> %d\n", rv);
-  if(rv != NGHTTP2_ERR_WOULDBLOCK && rv != 0) {
+  if(rv != 0) {
     mtevL(h2_debug, "http2 session_send(%p) %s\n", ctx, nghttp2_strerror(rv));
     goto full_shutdown;
   }
@@ -1195,11 +1197,12 @@ mtev_http2_session_drive(eventer_t e, int origmask, void *closure,
   mtev_http2_resume_all_unpaused_streams(ctx);
   rv = nghttp2_session_send(ctx->session);
   mtevL(h2_debug, "http2 drive -> %d\n", rv);
-  if(rv == NGHTTP2_ERR_WOULDBLOCK) {
-    mask |= EVENTER_WRITE;
-  }
-  else if(rv != 0) {
+  if(rv != 0) {
     mtevL(h2_debug, "http2 session_send(%p) %s\n", ctx, nghttp2_strerror(rv));
+    goto full_shutdown;
+  }
+  if(nghttp2_session_want_write(ctx->session)) {
+    mask |= EVENTER_WRITE;
   }
 
   if(nghttp2_session_want_read(ctx->session) == 0 &&
