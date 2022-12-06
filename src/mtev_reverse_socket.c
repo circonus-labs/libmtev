4d1fcdb4fef1dc555096ac899f4ff2fc655b0b14
Patrick Julien <patrick.julien@circonus.com>
2022-12-06 15:52:13 -0500

    fix

--- src/mtev_reverse_socket.c
+++ src/mtev_reverse_socket.c
@@ -546,7 +546,7 @@ mtev_reverse_socket_channel_handler(eventer_t e, int mask, void *closure,
       cct->parent->data.e = NULL;
     }
 
-    mtev_reverse_socket_deref(&cct->parent);
+    mtev_reverse_socket_deref(cct->parent);
     free(cct);
     return 0;
   }
