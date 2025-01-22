80e46f7d37f71757cc25b43505c088a556d17564
Philip Maddox <philip.maddox@circonus.com>
2025-01-22 16:48:58 +0000

    In progress, doesn't compile

--- src/modules/mtev_kafka.cpp
+++ src/modules/mtev_kafka.cpp
@@ -29,7 +29,51 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include "mtev_defines.h"
+#include "mtev_log.h"
+#include "mtev_hooks.h"
 #include "mtev_dso.h"
+#include "mtev_conf.h"
+#include "mtev_rand.h"
+#include "mtev_thread.h"
+
+#define CONFIG_KAFKA_IN_MQ "//network//mq[@type='kafka']"
+
+struct kafka_module_config {
+  kafka_module_config() : number_of_conns{0} {}
+  ~kafka_module_config() = default;
+
+  int number_of_conns;
+};
+
+static mtev_log_stream_t nlerr = nullptr;
+static mtev_log_stream_t nldeb = nullptr;
+static kafka_module_config *the_conf = nullptr;
+
+static kafka_module_config *get_config(mtev_dso_generic_t *self) {
+  if(the_conf) {
+    return the_conf;
+  }
+  the_conf = static_cast<kafka_module_config *>(mtev_image_get_userdata(&self->hdr));
+  if(the_conf) {
+    return the_conf;
+  }
+  the_conf = new kafka_module_config{};
+  mtev_image_set_userdata(&self->hdr, the_conf);
+  return the_conf;
+}
+
+static void
+init_conns(void) {
+  mtev_conf_section_t *mqs = mtev_conf_get_sections_read(MTEV_CONF_ROOT, CONFIG_KAFKA_IN_MQ,
+      &the_conf->number_of_conns);
+
+  if(the_conf->number_of_conns == 0) {
+    mtev_conf_release_sections_read(mqs, the_conf->number_of_conns);
+    return;
+  }
+  mtev_conf_release_sections_read(mqs, the_conf->number_of_conns);
+}
 
 static int
 kafka_driver_config(mtev_dso_generic_t *img, mtev_hash_table *options) {
@@ -38,6 +82,15 @@ kafka_driver_config(mtev_dso_generic_t *img, mtev_hash_table *options) {
 
 static int
 kafka_driver_init(mtev_dso_generic_t *img) {
+  auto conf = get_config(img);
+  nlerr = mtev_log_stream_find("error/kafka");
+  nldeb = mtev_log_stream_find("debug/kafka");
+
+  init_conns();
+  if (the_conf->number_of_conns == 0) {
+    mtevL(nlerr, "No kafka reciever setting found in the config!\n");
+    return 0;
+  }
   return 0;
 }
 
